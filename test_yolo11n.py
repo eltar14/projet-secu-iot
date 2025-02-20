f1dc452614eabaf17fa3c68216a4eb122aea608a
eltar14 <lebouant2@gmail.com>
2025-02-19 22:17:17 -0500

    yolo tests perfs

---
+++ test_yolo11n.py
@@ -0,0 +1,32 @@
+import cv2
+from ultralytics import YOLO
+
+def main(model_path, max_fps=10):
+    model = YOLO(model_path)
+
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Erreur : Impossible d'ouvrir la webcam.")
+        return()
+
+    cap.set(cv2.CAP_PROP_FPS, max_fps) # fixer les FPS de la cam
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Erreur : Impossible de lire l'image.")
+            break
+
+        results = model(frame)
+        annotated_frame = results.render()[0]
+        cv2.imshow("YOLO Detection", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main(model_path='models/yolo11n_ncnn_model')
