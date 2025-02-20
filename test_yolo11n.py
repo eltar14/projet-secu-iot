a1527058515c297f9037c29d5709b54f4c38dabe
eltar14 <lebouant2@gmail.com>
2025-02-19 23:57:24 -0500

    framerate fixed

--- test_yolo11n.py
+++ test_yolo11n.py
@@ -1,7 +1,8 @@
 import cv2
 from ultralytics import YOLO
+import time
 
-def main(model_path, max_fps=10):
+def main(model_path, max_fps=4):
     model = YOLO(model_path)
 
     cap = cv2.VideoCapture(0)
@@ -10,10 +11,11 @@ def main(model_path, max_fps=10):
         print("Erreur : Impossible d'ouvrir la webcam.")
         return()
 
-    cap.set(cv2.CAP_PROP_FPS, max_fps) # fixer les FPS de la cam
-
+    frame_delay = 1.0 / max_fps
 
     while True:
+        start_time = time.time()
+
         ret, frame = cap.read()
         if not ret:
             print("Erreur : Impossible de lire l'image.")
@@ -23,6 +25,10 @@ def main(model_path, max_fps=10):
         #annotated_frame = results.render()[0]
         result_frame = results[0].plot()
         cv2.imshow("YOLO Detection", result_frame)
+
+        elapsed_time = time.time() - start_time
+        sleep_time = max(0, int(frame_delay - elapsed_time))
+        time.sleep(sleep_time)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
