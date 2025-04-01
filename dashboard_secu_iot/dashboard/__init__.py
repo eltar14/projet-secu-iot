a3f4cd432ddd5be62ce95c8104de869b407c81cc
Elouaaaan <elouan.pennec@gmail.com>
2025-04-01 18:45:21 -0400

    add video to route

--- dashboard_secu_iot/dashboard/__init__.py
+++ dashboard_secu_iot/dashboard/__init__.py
@@ -25,6 +25,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import video
+    app.register_blueprint(video.bp)
+
     from . import dashboard
     app.register_blueprint(dashboard.bp)
     app.add_url_rule('/', endpoint='index')
