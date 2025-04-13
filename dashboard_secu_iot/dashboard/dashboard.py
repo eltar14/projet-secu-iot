fe7188982bcccd08b7e12171194b5f5926581f6f
Elouaaaan <elouan.pennec@gmail.com>
2025-04-13 18:26:52 -0400

    modif frenet decryption

--- dashboard_secu_iot/dashboard/dashboard.py
+++ dashboard_secu_iot/dashboard/dashboard.py
@@ -1,9 +1,12 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, send_from_directory
+    Blueprint, flash, g, redirect, render_template, request, url_for, send_from_directory, abort, Response, current_app
 )
+
+from cryptography.fernet import Fernet
 
 from dashboard.auth import login_required, user_exist
 from dashboard.db import get_db
+import os
 
 bp = Blueprint('dashboard', __name__)
 
@@ -23,7 +26,37 @@ def dashboard():
     return render_template('dashboard.html')
 
 
-@bp.route('/static/images/<path:filename>', methods=('GET', ))
+
+def decrypt_stream(path):
+    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
+    def generate():
+        with open(path, 'rb') as f:
+            encrypted_data = f.read()
+            try:
+                decrypted_data = fernet.decrypt(encrypted_data)
+            except Exception:
+                abort(500, description="Decryption failed.")
+            
+            chunk_size = 1024 * 1024
+            for i in range(0, len(decrypted_data), chunk_size):
+                yield decrypted_data[i:i + chunk_size]
+
+    return generate
+
+
+@bp.route('/static/images/<path:filename>')
 @login_required
-def serve_static(filename):
-    return send_from_directory('static/images', filename), 200
+def serve_decrypted_video(filename):
+    encrypted_path = os.path.join(os.getcwd(), 'dashboard', 'static', 'images', filename + '.enc')
+
+    if not os.path.exists(encrypted_path):
+        abort(404)
+
+    return Response(
+        decrypt_stream(encrypted_path),
+        mimetype='video/mp4',
+        headers={
+            "Content-Disposition": f"inline; filename={filename}",
+            "Accept-Ranges": "bytes"
+        }
+    )
