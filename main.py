2c631495f54ca37f5dc1a864b3324876cff59341
eltar14 <lebouant2@gmail.com>
2025-04-13 17:12:16 -0400

    video mp4v to avc1

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main(model_path, max_fps=4, no_detection_timeout=2):
     video_filename = None
     video_path = None
 
-    fourcc = cv2.VideoWriter_fourcc(*'H264')  # codec vidéo
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
 
     while True:
         start_time = time.time()
